6b7c343e23579ded705a3dc7b6aeb383128e1da8
Noel Rojas <narojasgalviz@gmail.com>
2021-09-17 14:50:44 -0400

    Adding GroundingList view and controller

--- HackathonProject/src/controllers/MainController.java
+++ HackathonProject/src/controllers/MainController.java
@@ -1,10 +1,19 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -14,7 +23,15 @@ public class MainController {
 
     @FXML
     void startProgram(ActionEvent event) {
-
+    	RadioButton selected = (RadioButton)options.getSelectedToggle();
+    	String selection = selected.getText();
+    	String fxmlFile = "";
+    	
+    	switch(selection) {
+    	case "Option 1":
+    		fxmlFile = "../views/GroundingList.fxml";
+    		changeScreen(event, fxmlFile, "Grounding List");
+    	}
     }
 
     @FXML
@@ -22,4 +39,21 @@ public class MainController {
 
     }
 
+    void changeScreen(ActionEvent event, String fxmlFile, String title) {
+    	try {
+			Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../views/Main.fxml"));
+			//Load the view for the type of machine chosen
+			AnchorPane center = (AnchorPane)FXMLLoader.load(getClass().getResource(fxmlFile));
+			//Set the center of the border pane depending on the type of machine chosen
+			root.setCenter(center);
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("../assets/styleSheets/application.css").toExternalForm());
+			primaryStage.setTitle(title);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+    }
 }
