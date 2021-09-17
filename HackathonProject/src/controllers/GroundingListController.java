3ec7b2953da988f3f1e19e25aff96deff0a13bad
Noel Rojas <narojasgalviz@gmail.com>
2021-09-17 16:22:25 -0400

    Adding articles and breathing views and their corresponding controllers

---
+++ HackathonProject/src/controllers/GroundingListController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class GroundingListController {
+	
+	
+
+    @FXML private ToggleGroup grounding;
+    @FXML private Button groundingSelection;
+	
+	@FXML
+	void startGrounding(ActionEvent event) {
+		
+	}
+	
+	@FXML 
+	void goBack(ActionEvent event) {
+		try {
+			Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../views/Main.fxml"));
+			//Load the view for the type of machine chosen
+			AnchorPane center = (AnchorPane)root.getCenter();
+			//Set the center of the border pane depending on the type of machine chosen
+			root.setCenter(center);
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("../assets/styleSheets/application.css").toExternalForm());
+			primaryStage.setTitle("Grounding");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
