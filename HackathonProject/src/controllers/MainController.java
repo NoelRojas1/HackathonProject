e1d1f98b5ce8dfe0f32cd8b25b53dce3a37662b4
Noel Rojas <narojasgalviz@gmail.com>
2021-09-17 14:09:07 -0400

    Adding main view

--- HackathonProject/src/controllers/MainController.java
+++ HackathonProject/src/controllers/MainController.java
@@ -1,5 +1,25 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
 public class MainController {
-	
+
+    @FXML private Text appName;
+    @FXML private ToggleGroup options;
+    @FXML private Button btnStart, btnRandom;
+
+    @FXML
+    void startProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startProgramRandom(ActionEvent event) {
+
+    }
+
 }
